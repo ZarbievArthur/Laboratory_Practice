273e0404c395feda1b981821601b6b788aa91b28
Zarbiev Arthur <Zarbiev.Arthur@8E41.ru>
2026-09-21 14:52:35 +0800

    Hoooray I did it I did power on lamp for CMSIS

--- Core/Inc/init.h
+++ Core/Inc/init.h
@@ -1,42 +1,5 @@
-#include <stdint.h>
-#define READ_GPIO_C13               (*(uint32_t*)(0x40020800UL + 0x10UL) & 0x2000UL)
-#define SET_GPIO_B7                 (*(uint32_t*)(0x40020400UL + 0x18UL) = 0x80) 
-#define RESET_GPIO_B7               (*(uint32_t*)(0x40020400UL + 0x18UL) = 0x00800000)
+#include "../../CMSIS/Devices/STM32F4xx/Inc/stm32f4xx.h"
+#include "../../CMSIS/Devices/STM32F4xx/Inc/STM32F429xx/stm32f429xx.h"
 
-#define RCC_GPIO_EN                 *(uint32_t*)(0x40023800UL + 0x30UL)
-
-#define GPIOB_MODER                 *(uint32_t*)(0x40020400UL + 0x00UL)
-#define GPIOB_OTYPER                *(uint32_t*)(0x40020400UL + 0x04UL)
-#define GPIOB_OSPEEDR               *(uint32_t*)(0x40020400UL + 0x08UL)
-#define GPIOB_PUPDR                 *(uint32_t*)(0x40020400UL + 0x0CUL)
-
-#define GPIOC_MODER                 *(uint32_t*)(0x40020800UL + 0x00UL)
-#define GPIOC_OTYPER                *(uint32_t*)(0x40020800UL + 0x04UL)
-#define GPIOC_OSPEEDR               *(uint32_t*)(0x40020800UL + 0x08UL)
-#define GPIOC_PUPDR                 *(uint32_t*)(0x40020800UL + 0x0CUL)
-
-#define RCC_GPIOB_EN                0x02UL
-#define RCC_GPIOC_EN                0x04UL
-
-#define GPIOB_MODE_PIN7_OUT         0x4000UL
-#define GPIOB_OTYPE_PIN7_PP         0x00000UL
-#define GPIOB_OSPEED_PIN7_MID       0x4000UL
-#define GPIOB_PUPDR_PIN7_NOPUPD     0x00000UL
-
-#define GPIOC_MODE_PIN7_IN          0x00000000UL
-#define GPIOC_OTYPE_PIN7_PP         0x00000000UL
-#define GPIOC_OSPEED_PIN7_MID       0x08000000UL
-#define GPIOC_PUPDR_PIN7_NOPUPD     0x00000000UL
-
-#define GPIOB_BSRR                  (*(uint32_t*)(0x40020400UL + 0x18UL))
-
-#define GPIOB_BSRR_PIN7_SET         0x00000080UL 
-#define GPIOB_BSRR_PIN7_RESET       0x00800000UL
-
-#define SET_BIT(REG, BIT)           (REG |= BIT)
-
-#define GPIOC_IDR                   (*(uint32_t*)(0x40020800UL + 0x10UL))
-#define GPIOC_IDR_PIN13             0x2000UL
-#define READ_BIT(REG, BIT)          (REG & BIT)
 
 void GPIO_Ini(void);
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,14 +1,18 @@
 #include "init.h"
 
+uint8_t flag;
+
 int main(void) {
 
     GPIO_Ini();
     while(1){
-        if(READ_BIT(GPIOC_IDR, GPIOC_IDR_PIN13) != 0){
-            SET_BIT(GPIOB_BSRR, GPIOB_BSRR_PIN7_SET); 
+        if(READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13) != 0){
+            flag = 1;
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7); 
         }
         else{
-            SET_BIT(GPIOB_BSRR, GPIOB_BSRR_PIN7_RESET); 
+            flag = 0;
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7); 
         }
     }
 }
